0503e01131d9afb7f97dbfb6faf76ca30bb5a264
jlucasrods <jlucasrods@gmail.com>
2021-04-27 12:15:05 -0300

    add logo

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import logo from './assets/logo.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{width: 305, height: 159}} />
       <Text style={{color: '#888', fontSize: 18}}>Para compartilhar uma foto do seu telefone com um amigo, basta pressionar o botão abaixo!</Text>
       <StatusBar style="auto" />
     </View>
